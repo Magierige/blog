d42bb6b00357dbecc56e9e2831398345e8b02612
Ronald <r.vandevelde01@student.alfa-college.nl>
2024-02-01 16:06:56 +0100

    0.8.3
    check en weergave van je eigen likes/dislikes

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -16,16 +16,16 @@ Edit blog
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                 {!! $blog->content !!}
                 <p>author: {{$blog->author}} </p>
-                <p>like: {{$blog->like}} </p>
-                <p>dislike: {{$blog->dislike}} </p>
+                <p class="@if($blog->ulike) text-green-500 @endif">like: {{$blog->like}} </p>
+                <p class="@if(isset($blog->ulike) && $blog->ulike === false) text-red-500 @endif">dislike: {{$blog->dislike}} </p>
                 <div class="flex flex-row  flex-wrap">
                     @foreach($blog->reactions as $comment)
                         <div class=" basis-1/3">
                             <div class="px-4 py-2 m-2 text-gray-800 dark:text-gray-200 bg-gray-300 dark:bg-gray-700 rounded-lg">
                                 <p>author: {{$comment->author}} </p>
                                 <p>comment: {{$comment->message}} </p>
-                                <p>like: {{$comment->like}} </p>
-                                <p>dislike: {{$comment->dislike}} </p>
+                                <p class="@if($comment->ulike) text-green-500 @endif">like: {{$comment->like}} </p>
+                                <p class="@if(isset($comment->ulike) && $comment->ulike === false) text-red-500 @endif">dislike: {{$comment->dislike}} </p>
                             </div>
                         </div>
                     @endforeach
